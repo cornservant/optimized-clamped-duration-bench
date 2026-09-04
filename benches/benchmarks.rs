4db7a8d23de49a2221379907ff2f5f77f40f158b
Long Huynh Huu <long@putputlaw.de>
2026-09-04 22:32:39 +0200

    move generate_input() into lib

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,82 +1,74 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use optimized_clamped_duration_bench::*;
 
-fn generate_input() -> Vec<(f64, f64, f64, f64, f64)> {
-    use rand_distr::Distribution;
-    use rand_distr::Exp;
-    let mut rng = rand::rng();
-    let dist_r = Exp::new(1.0 / 1.0).unwrap();
-    let dist_s = Exp::new(1.0 / 1.0).unwrap();
-    let dist_e = Exp::new(1.0 / 0.0001).unwrap();
-    let dist_v = Exp::new(1.0 / 2.0).unwrap();
-    let dist_t = Exp::new(1.0 / 10.0).unwrap();
-    (0..500)
-        .map(|_| {
-            (
-                dist_r.sample(&mut rng),
-                dist_s.sample(&mut rng),
-                dist_e.sample(&mut rng),
-                dist_v.sample(&mut rng),
-                dist_t.sample(&mut rng),
-            )
-        })
-        .collect()
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
     let input = generate_input();
-    let from = 0.0;
 
     let mut group = c.benchmark_group("day01::solve1");
     group.bench_with_input("baseline", &input, |b, input| {
         b.iter(|| {
-            input
-                .iter()
-                .copied()
-                .for_each(|(ratio, stiffness, eps, initial_velocity, to)| {
-                    let params = baseline::SpringParams::new(ratio, stiffness, eps);
+            input.iter().copied().for_each(
+                |Params {
+                     damping_ratio,
+                     stiffness,
+                     epsilon,
+                     initial_velocity,
+                     from,
+                     to,
+                 }| {
                     let spring = baseline::Spring {
                         from,
                         to,
                         initial_velocity,
-                        params,
+                        params: baseline::SpringParams::new(damping_ratio, stiffness, epsilon),
                     };
                     let _ = spring.clamped_duration();
-                })
+                },
+            )
         })
     });
     group.bench_with_input("commit1", &input, |b, input| {
         b.iter(|| {
-            input
-                .iter()
-                .copied()
-                .for_each(|(ratio, stiffness, eps, initial_velocity, to)| {
-                    let params = commit1::SpringParams::new(ratio, stiffness, eps);
+            input.iter().copied().for_each(
+                |Params {
+                     damping_ratio,
+                     stiffness,
+                     epsilon,
+                     initial_velocity,
+                     from,
+                     to,
+                 }| {
                     let spring = commit1::Spring {
                         from,
                         to,
                         initial_velocity,
-                        params,
+                        params: commit1::SpringParams::new(damping_ratio, stiffness, epsilon),
                     };
                     let _ = spring.clamped_duration();
-                })
+                },
+            )
         })
     });
     group.bench_with_input("commit2", &input, |b, input| {
         b.iter(|| {
-            input
-                .iter()
-                .copied()
-                .for_each(|(ratio, stiffness, eps, initial_velocity, to)| {
-                    let params = commit2::SpringParams::new(ratio, stiffness, eps);
+            input.iter().copied().for_each(
+                |Params {
+                     damping_ratio,
+                     stiffness,
+                     epsilon,
+                     initial_velocity,
+                     from,
+                     to,
+                 }| {
                     let spring = commit2::Spring {
                         from,
                         to,
                         initial_velocity,
-                        params,
+                        params: commit2::SpringParams::new(damping_ratio, stiffness, epsilon),
                     };
                     let _ = spring.clamped_duration();
-                })
+                },
+            )
         })
     });
     group.finish();
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
 pub mod baseline;
 pub mod commit1;
 pub mod commit2;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Params {
+    pub damping_ratio: f64,
+    pub stiffness: f64,
+    pub epsilon: f64,
+    pub from: f64,
+    pub to: f64,
+    pub initial_velocity: f64,
+}
+
+pub fn generate_input() -> Vec<Params> {
+    use rand_distr::Distribution;
+    use rand_distr::Exp;
+    let mut rng = rand::rng();
+    let dist_d = Exp::new(1.0 / 2.0).unwrap();
+    let dist_s = Exp::new(1.0 / 2.0).unwrap();
+    let dist_e = Exp::new(1.0 / 0.0001).unwrap();
+    let dist_v = Exp::new(1.0 / 2.0).unwrap();
+    let dist_t = Exp::new(1.0 / 10.0).unwrap();
+    (0..500)
+        .map(|_| Params {
+            damping_ratio: dist_d.sample(&mut rng),
+            stiffness: dist_s.sample(&mut rng),
+            epsilon: dist_e.sample(&mut rng),
+            initial_velocity: dist_v.sample(&mut rng),
+            from: 0.0,
+            to: dist_t.sample(&mut rng),
+        })
+        .collect()
+}
