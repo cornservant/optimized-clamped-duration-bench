962638c7113d76ca39be9d22bb5a57c98cceb5c1
Long Huynh Huu <long@putputlaw.de>
2026-09-04 22:34:25 +0200

    chore: remove julia deps

--- flake.nix
+++ flake.nix
@@ -15,18 +15,12 @@
       formatter = pkgs: pkgs.nixfmt;
       devShells = pkgs: {
         default = pkgs.mkShell {
-          LD_LIBRARY_PATH =
-            pkgs.lib.makeLibraryPath [
-              pkgs.julia
-            ]
-            + ":${pkgs.julia}/lib/julia";
           packages = [
             pkgs.rustc
             pkgs.cargo
             pkgs.rust-analyzer
             pkgs.rustfmt
             pkgs.clippy
-            pkgs.julia
           ];
         };
       };
